a788e8ce02204cd08e7fe9cfa697885aee00811f
Gabriel Seabra <gmatos@hotmail.com.br>
2026-03-23 23:50:10 -0300

    feat: adiciona seção hero com título e imagem animada

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 export default function home() {
   return (
     <div>
+
+      {/* NAVBAR */}
       <header className="w-full bg-black text-white p-4 flex justify-between items-center">
 
         <h1 className="text-x1 font-bold">
@@ -14,8 +16,29 @@ export default function home() {
           <a href="#" className="hover:text-gray-400">Skills</a>
           <a href="#" className="hover:text-gray-400">Redes</a>
         </nav>
-
       </header>
+
+       {/* HERO */}
+      <section className="h-[80vh] flex items-center justify-center gap-16">
+
+        {/* TEXTO */}
+        <div>
+          <h2 className="text-5x1 font-bold leading-tight">
+            Gabriel Seabra
+          </h2>
+
+          <p className="text-2x1 mt-3 text-gray-500">
+            Desenvolvedor Full Stack
+          </p>
+        </div>
+
+      {/* IMAGEM */}
+      <div className="animate-bounce">
+        <img src="https://cdn-icons-png.flaticon.com/512/4712/4712109.png" width={220}/>
+      </div>
+
+      </section>
+
     </div>
   );
 }
